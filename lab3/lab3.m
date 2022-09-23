174c9b32c5f5c9406d5c5c0cdfede98e96a7abe8
axidex <Stepworm@yandex.ru>
2022-09-24 02:20:04 +0300

    Update lab3.m

--- lab3/lab3.m
+++ lab3/lab3.m
@@ -1,164 +1,80 @@
 function lab3(L,n)
-  nMax = 20;
- N = 1000;
+ nMax = 30;
+ N = 250;
 X = linear_grid(L, N);
 Y = zeros(N,1);
+Ytest = zeros(nMax,1);
 for i=1:N
   Y(i) =f(X(i));
 endfor;
 
-#####
-
-
-XT = T_grid(L, n);
-YT = zeros(n,1);
-
-Xl = length(XT);
-for  i= 1:Xl
-  YT(i) = f(XT(i));
-endfor
-#XT
-#YT
-YTi = zeros(N, 1);
-for i=1:N
-  YTi(i) = interpolate(XT,YT,X(i));
-endfor
-#YTi
-###########
-
-XT2 = T_grid(L, n+10);
-YT2 = zeros(n+10,1);
-
-
-for  i= 1:length(XT2)
-  YT2(i) = f(XT2(i));
-endfor
-#XT
-#YT
-YT2i = zeros(N, 1);
-for i=1:N
-  YT2i(i) = interpolate(XT2,YT2,X(i));
-endfor
-######
-
-XT3 = T_grid(L, n+20);
-YT3 = zeros(n+20,1);
-
-
-for  i= 1:length(XT3)
-  YT3(i) = f(XT3(i));
-endfor
-#XT
-#YT
-YT3i = zeros(N, 1);
-for i=1:N
-  YT3i(i) = interpolate(XT3,YT3,X(i));
-endfor
-######
-XL = linear_grid(L, n);
-YL = zeros(n,1);
-
-for  i= 1:Xl
-  YL(i) = f(XL(i));
-endfor
-#XL
-#YL
-YLi = zeros(N, 1);
-for i=1:N
-  YLi(i) = interpolate(XL,YL,X(i));
-endfor
-#YLi
-#########
-XL2 = linear_grid(L, n+10);
-YL2 = zeros(n+10,1);
-
-for  i= 1:length(XL2)
-  YL2(i) = f(XL2(i));
-endfor
-#XL
-#YL
-YL2i = zeros(N, 1);
-for i=1:N
-  YL2i(i) = interpolate(XL2,YL2,X(i));
-endfor
-##########
-XL3 = linear_grid(L, n+20);
-YL3 = zeros(n+20,1);
-
-for  i= 1:length(XL3)
-  YL3(i) = f(XL3(i));
-endfor
-#XL
-#YL
-YL3i = zeros(N, 1);
-for i=1:N
-  YL3i(i) = interpolate(XL3,YL3,X(i));
-endfor
-##########
-
-errors = zeros(n,1);
-errors2 = zeros(n,1);
-errors3 = zeros(n,1);
-errors4 = zeros(n,1);
-errors5 = zeros(n,1);
-errors6 = zeros(n,1);
-#########
-for i = 1:N
-  errors(i) = Y(i) - YTi(i);
-  errors2(i) = Y(i) - YLi(i);
-  errors3(i) = Y(i) - YT2i(i);
-  errors4(i) = Y(i) - YL2i(i);
-  errors5(i) = Y(i) - YT3i(i);
-  errors6(i) = Y(i) - YL3i(i);
-
-endfor
-#errors
-#errors2
-
+for i=1:nMax
+  Ytest(i) = f(X(i));
+endfor;
 
 #####
 
-figure(1)
-
-plot(X, Y, X, YTi, X, YT2i, X, YT3i)
-
-grid on
-
-figure(2)
-
-plot(X, Y, X, YLi, X, YL2i, X, YL3i)
-
-grid on
-
-
-figure(3)
-plot (1:N, log10(abs(errors)), 1:N, log10(abs(errors3)), 1:N, log10(abs(errors5)))
-grid on
-
-figure (4)
-plot (1:N, log10(abs(errors2)), 1:N, log10(abs(errors4)), 1:N, log10(abs(errors6)))
-grid on
-
-
-##########
 maxT = zeros(nMax,1);
 sumT = zeros(nMax,1);
+
 for i = 1:nMax
   XTn = T_grid(L, nMax);
   YTn = zeros(nMax,1);
 
-  for  i= 1:length(XTn)
-    YTn(i) = f(XTn(i));
+  for  j= 1:length(XTn)
+    YTn(i) = f(XTn(j));
   endfor
   YTni = zeros(N, 1);
-  for i=1:N
-    YTni(i) = interpolate(XTn,YTn,X(i));
+  for j=1:N
+    YTni(j) = interpolate(XTn,YTn,X(j));
   endfor
   maxT(i) = max(YTni);
   sumT(i) = sum(YTni)/N;
 endfor
+maxT;
+sumT;
+
+
+
+########
+
+maxL = zeros(nMax,1);
+sumL = zeros(nMax,1);
+for i = 1:nMax
+  XLn = linear_grid(L, nMax);
+  YLn = zeros(nMax,1);
+
+  for  j= 1:length(XLn)
+    YLn(j) = f(XLn(j));
+  endfor
+  YLni = zeros(N, 1);
+  for j=1:N
+    YLni(j) = interpolate(XLn,YLn,X(j));
+  endfor
+  maxL(i) = max(YLni);
+  sumL(i) = sum(YLni)/N;
+endfor
+#####
+errors = zeros(nMax,1);
+errors2 = zeros(nMax,1);
+errors3 = zeros(nMax,1);
+errors4 = zeros(nMax,1);
+
+#########
+for i = 1:nMax
+  errors(i) = Ytest(i) - maxL(i);
+  errors2(i) = Ytest(i) - sumL(i);
+  errors3(i) = Ytest(i) - maxT(i);
+  errors4(i) = Ytest(i) - sumT(i);
+endfor
+##########
+
 figure (5)
-plot (1:nMax, log10(abs(maxT)), 1:nMax, log10(abs(sumT)))
+plot (1:nMax, log10(abs(errors3)), 1:nMax, log10(abs(errors4)))
+grid on
+
+figure (6)
+plot (1:nMax, log10(abs(errors)), 1:nMax, log10(abs(errors2)))
 grid on
 
 
